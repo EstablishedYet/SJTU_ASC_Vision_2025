3d7e58dab2b8f1e3484fe8cc55d8fe8958e41385
Established <xushibo811@outlook.com>
2025-09-05 10:47:53 +0800

    update pos.py to this generation's version

--- pos.py
+++ pos.py
@@ -1,10 +1,10 @@
 class Imgdata(object):
-    def __init__(self, pos, coordinate, num, time, yaw,cropTensorList,speed):  # pos是飞机三维坐标，coordinate是天井在照片的二维坐标
+    def __init__(self, pos,num, yaw,cropTensorList,speed):  # pos是飞机三维坐标，coordinate是天井在照片的二维坐标
         # self.image = image  # 图像
         self.pos = pos  # x, y, z
-        self.coordinate = coordinate  # 二维坐标
+        # self.coordinate = coordinate  # 二维坐标
         self.num = num  # 识别到的数字
-        self.time = time  # 时间戳
+        # self.time = time  # 时间戳
         self.yaw = yaw
         self.cropTensorList = cropTensorList
         self.speed = speed
@@ -13,20 +13,20 @@ class Imgdata(object):
     def make_struct(image, pos, coordinate, num, time, yaw,cropTensorList):
         return Imgdata(image, pos, coordinate, num, time, yaw)
 
-    def get_pos_and_coordinate(self):
-        return self.pos, self.coordinate
+    # def get_pos_and_coordinate(self):
+    #     return self.pos, self.coordinate
 
     def get_pos(self):
         return self.pos
 
-    def get_coordinate(self):
-        return self.coordinate
+    # def get_coordinate(self):
+    #     return self.coordinate
 
     def get_num(self):
         return self.num
 
-    def grt_time(self):
-        return self.time
+    # def grt_time(self):
+    #     return self.time
 
     def get_yaw(self):
         return self.yaw
